c1aac335d468cff244a0969488d19c3a480844de
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-09-26 20:12:03 +0800

    增加 hardlink 方式創建分類

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
-dl_renamer
-===============
-總之就是換名子的，格式不能改就是\[*$RJNUMBER*\]\[*$CLUB*\] *$NAME*
+# dl_renamer 1.0.1
 
-會順便問你要不要下載圖片，要不要都可
+總之就是換名子的，格式能改，去config.ini看格式  
+增加了hardlink連結的分類，如果不清楚hardlink是甚麼可以去google baidu  
+簡單來說就是，直接連結到硬碟位置，檔案管理員會認為他是實體檔案，事實上他會是連結。  
+想要查看檔案硬連結(hardlink)的數量，可以在cmd上輸入 sh
 
-簡單使用
-=============
+## 簡單使用
 
-如果你是windows使用者
-
+如果你是windows使用者  
 就執行位於*dist*下的*exe*檔案
 
 ![before](/usage_pic/before.PNG)
 ![middle](/usage_pic/middle.PNG)
 ![after](/usage_pic/after.PNG)
 
-如果你是mac那就直接上環境吧
-
-    pip install -r requirements.txt
-    python dl_renamer.py
-
-理論上在那個目錄下的RJNUMBER會被更名
+如果你是mac那就直接上環境吧，那抱歉 mac 沒辦法hardlink你可以選擇不要hardlink  
+不過我沒有mac會怎麼樣我就不知了
--- dl_renamer.py
+++ dl_renamer.py
@@ -6,42 +6,81 @@ import configparser
 import requests
 from bs4 import BeautifulSoup
 
-_REQ_LINK = "http://www.dlsite.com/maniax/work/=/product_id/{}.html"
+DOMAIN = "http://www.dlsite.com"
+_REQ_LINK = DOMAIN + "/maniax/work/=/product_id/{}.html"
 RENAME_PATTERN = "[{rjnumber}][{maker_name}] {name} {genre}"
 
 
 def main():
-    req_list = [req for req in os.listdir(".") if re.search(
-        r"([Rr][Jj][0-9]+)", req) is not None]
-    print("識別檔案:{}\n確定要執行嗎(y)?".format(req_list), end="")
-    if input() != 'y':
-        return
-    
+    req_list = _parse_file(os.listdir("."))
+    print("識別檔案:{}\n需要創建 hard Link 分類嗎(y)?".format(req_list), end="")
+    HL = input()
+
     for req in req_list:
         find = re.search(r"([rjRJ]{2}[0-9]+)", req)
         target = find.group(1).upper()
-        print("找尋{}...".format(target), end="")
-        
+        print("找尋{}.....".format(target), end="")
+
         try:
             work = Work(target)
             # change name
-            if os.path.isdir(req):
-                change_name = _combine_name(work)
-            else:
-                change_name = _combine_name(work) + "." + req.split(".")[-1]
-            os.rename(req, change_name)
-            print("成功! 更名為 {}".format(change_name))
+            file_name = _combine_name(work)
+            if not os.path.isdir(req):
+                file_name += "." + req.split(".")[-1]
+            jpg_name = _combine_name(work) + ".jpg"
+            os.rename(req, file_name)
+            print("\t完成! 更名為 {}".format(file_name))
             # download img
             print("下載{}img中...".format(target), end="")
             with requests.get(work.image_link, stream=True) as req:
-                with open(_combine_name(work) + ".jpg", "wb") as f:
+                with open(jpg_name, "wb") as f:
                     for chunk in req.iter_content(chunk_size=1024):
                         f.write(chunk)
-            print("完成")
+            print("\t完成")
+
+            # hard link
+            if HL == 'y':
+                print("創建 hard link 連結...", end="")
+                for link_target in work.get_category_list():
+                    destination = "{type}{sep}".format(type=_safe_name(link_target[0]), sep=os.sep)
+                    if isinstance(link_target[1], list):
+                        for sub_link_target in link_target[1]:
+                            sub_destination = destination + "{sub_type}{sep}".format(sub_type=_safe_name(sub_link_target), sep=os.sep)
+                            os.makedirs(sub_destination, exist_ok=True)
+                            os.link(jpg_name, sub_destination + jpg_name)
+                            _hardlink_allow_floder(file_name, sub_destination + file_name)
+                    else:
+                        sub_destination = destination + "{sub_type}{sep}".format(sub_type=_safe_name(link_target[1]), sep=os.sep)
+                        os.makedirs(sub_destination, exist_ok=True)
+                        os.link(jpg_name, sub_destination + jpg_name)
+                        _hardlink_allow_floder(file_name, sub_destination + file_name)
+                print('\t完成')
+                # os.link(change_name, maker_location + change_name)
+
         except Exception as e:
+            with open("fail.txt", 'a', encoding='utf-8') as f:
+                f.write("{} 失敗..".format(target))
+                f.write("cause: {}\n".format(str(e)))
             print("失敗{} 原因:{}".format(target, e))
     print("等待確認~~", end="")
     input()
+
+
+def fuck(work, genre=None):
+    change_name = _combine_name(work)
+    picture = "".join(change_name.split(".")[0::-1]) + ".jpg"
+    if genre is not None:
+        name = "製作"
+        fuck_str = genre
+    else:
+        name = "分類"
+        fuck_str = work.maker
+    location = "{name}{sep}{fuck_str}{sep}".format(
+        name=name, sep=os.sep, fuck_str=fuck_str)
+    os.makedirs(location, exist_ok=True)
+    os.link(picture, location + picture)
+    _hardlink_allow_floder(
+        change_name, location + change_name)
 
 
 class Work():
@@ -56,6 +95,8 @@ class Work():
 
     def get_image_link(self):
         tag = self.bf.find("meta", {"name": "twitter:image:src"})
+        if tag.attrs["content"] == "/images/web/home/no_img_main.gif":
+            return DOMAIN + tag.attrs["content"]
         return tag.attrs["content"]
 
     def get_maker(self):
@@ -74,14 +115,23 @@ class Work():
     def get_rjnumber(self):
         return self.rjnumber
 
+    def get_category_list(self):
+        return [
+            ("種類", self.get_genre()),
+            ("製作社團", self.get_maker())
+        ]
+
 
 # @user_interface
 def loading_config():
     global RENAME_PATTERN
     config = configparser.ConfigParser()
     if os.path.isfile("config.ini"):
-        config.read("config.ini", encoding='utf-8')
-        RENAME_PATTERN = config["DEFAULT"]["rename_pattern"]
+        try:
+            config.read("config.ini", encoding='utf-8')
+            RENAME_PATTERN = config["DEFAULT"]["rename_pattern"]
+        except Exception:
+            print("config.ini dead.  Relive it or del it.")
     else:
         config["DEFAULT"] = {"rename_pattern": RENAME_PATTERN}
         with open("config.ini", 'w', encoding='utf-8') as configfile:
@@ -99,6 +149,12 @@ def _safe_name(name: str()):
     return name
 
 
+def _parse_file(file_list):
+    parsed_file_list = [req for req in file_list if re.search(
+        r"([Rr][Jj][0-9]+)", req) is not None and req.split(".")[-1] != "jpg"]
+    return parsed_file_list
+
+
 def _combine_name(work: Work):
     return _safe_name(
         RENAME_PATTERN.format(
@@ -107,5 +163,34 @@ def _combine_name(work: Work):
             name=work.name,
             genre=work.genre))
 
+
+def _hardlink_allow_floder(src, loc):
+    if os.path.isdir(src):
+        os.makedirs(loc, exist_ok=True)
+        hardlink_targets = os.listdir(src)
+        for hardlink_target in hardlink_targets:
+            _hardlink_allow_floder(
+                src + os.sep + hardlink_target, loc + os.sep + hardlink_target)
+    else:
+        try:
+            os.link(src, loc)
+        except FileExistsError:
+            pass
+
+# 這是給其他專案用的
+def parse_filename_to_infodict(filename):
+    pattern = re.escape(RENAME_PATTERN)\
+        .replace(r"\{rjnumber\}", r"(?P<rjnumber>RJ\d+)")\
+        .replace(r"\{maker_name\}", r"(?P<maker_name>.+)")\
+        .replace(r"\{name\}", r"(?P<name>.+)")\
+        .replace(r"\{genre\}", r"\[(?P<genre>'.+')\]")
+    ret = re.search(pattern, filename)
+    genre = ret.group("genre").replace("'", "").split(", ")
+    rem = ret.groupdict()
+    rem.update({"genre": genre})
+    return rem
+
+
 loading_config()
-main()
+if __name__ == "__main__":
+    main()
